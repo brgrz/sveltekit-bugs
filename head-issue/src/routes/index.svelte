4cc4fc96e391c06ecd69f06b0d77efa94bcdc4bb
brgrz <marko@centiva.si>
2021-06-17 08:55:05 +0200

    initial commit

---
+++ head-issue/src/routes/index.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.43/dist/themes/base.css"
+	/>
+	<script
+		type="module"
+		src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.43/dist/shoelace.js"></script>
+</svelte:head>
